864b69c22c8cc52645d6fb2749eb3d1871b1fbfa
Murat Guchetl <gmurka@gmail.com>
2025-11-04 22:52:17 +0300

    feat: Added sakkada/reset.lua extra to allow resetting lazyvim to personal defaults
    Added reset.lua with customized plugins settings.
    Added reset section in built-in keymaps.lua with reset.lua extra existence condition.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.tbl_contains(LazyVim.config.json.data.extras, "plugins.extras.self.reset") then
+  local map = LazyVim.safe_keymap_set
+  --
+  -- better up/down
+  vim.keymap.del("n", "j")
+  vim.keymap.del("n", "<Down>")
+  vim.keymap.del("n", "k")
+  vim.keymap.del("n", "<Up>")
+
+  -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+  vim.keymap.del("n", "n")
+  vim.keymap.del("x", "n")
+  vim.keymap.del("o", "n")
+  vim.keymap.del("n", "N")
+  vim.keymap.del("x", "N")
+  vim.keymap.del("o", "N")
+
+  vim.keymap.del({ "n", "t" }, "<c-_>")
+  vim.keymap.del({ "n", "t" }, "<c-/>")
+
+  map("t", ";j", "<c-\\><c-n>", { noremap = true })
+end
---
+++ lua/plugins/extras/sakkada/reset.lua
@@ -0,0 +1,86 @@
+return {
+  -- desc = "Mini.files customization, preview window width",
+  { "nvim-mini/mini.pairs", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      -- Taken from https://github.com/folke/snacks.nvim/pull/421
+
+      --- Toggle all terminal windows.
+      --- If terminal windows exist:
+      ---   - If any are open, close all open terminals
+      ---   - If none are open, open all existing terminals
+      --- If no terminals exist:
+      ---   - Create a terminal window
+      --- `cmd` ans `opts` are optional and will be used
+      --- to create a terminal if needed.
+      ---@param cmd? string | string[]
+      ---@param opts? snacks.terminal.Opts
+      function Snacks.terminal.toggle_all(cmd, opts)
+        local terminals = Snacks.terminal.list()
+        local existing_terminals = {}
+        for _, term in pairs(terminals) do
+          if term and term:buf_valid() then
+            table.insert(existing_terminals, term)
+          end
+        end
+
+        if #existing_terminals > 0 then
+          local any_open = false
+          for _, term in ipairs(existing_terminals) do
+            if term:win_valid() then
+              any_open = true
+              break
+            end
+          end
+
+          if any_open then
+            for _, term in ipairs(existing_terminals) do
+              term:hide()
+            end
+          else
+            for _, term in ipairs(existing_terminals) do
+              term:show()
+            end
+          end
+        else
+          Snacks.terminal.toggle(cmd, opts)
+        end
+      end
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        interactive = false, -- alias for auto_insert, start_insert, auto_close
+        start_insert = true,
+        win = {
+          keys = {
+            -- in nvim-data/lazy/LazyVim/lua/lazyvim/plugins/util.lua:
+            --   nav_h, nav_j, nav_k, nav_l, hide_slash, hide_underscore
+            -- in nvim-data/lazy/snacks.nvim/lua/snacks/terminal.lua:
+            --   q, gf, term_normal
+            nav_h = false,
+            nav_j = false,
+            nav_k = false,
+            nav_l = false,
+            hide_slash = false,
+            hide_underscore = false,
+            term_normal = false,
+          },
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>/", false },  -- disable grep (root dir)
+      { "<c-_><c-_>", function() Snacks.terminal.toggle(nil, { win = { height = 0.5 } }) end, desc = "Toggle terminal", mode = { "n", "t" } },
+      { "<c-_><c-a>", function() Snacks.terminal.toggle_all(nil, { win = { height = 0.5 } }) end, desc = "Toggle all terminals", mode = { "n", "t" } },
+      { "<c-_>1", "1<cmd>lua Snacks.terminal.toggle(nil, { win = { height = 0.5 } })<cr>", desc = "Toggle terminal 1" },
+      { "<c-_>2", "2<cmd>lua Snacks.terminal.toggle(nil, { win = { height = 0.5 } })<cr>", desc = "Toggle terminal 2" },
+      { "<c-_>3", "3<cmd>lua Snacks.terminal.toggle(nil, { win = { height = 0.5 } })<cr>", desc = "Toggle terminal 3" },
+      { "<c-_>4", "4<cmd>lua Snacks.terminal.toggle(nil, { win = { height = 0.5 } })<cr>", desc = "Toggle terminal 4" },
+    },
+  },
+}
