e4fa4770f4c0a3da56d7586b7b60dc9978f07749
Murat Guchetl <gmurka@gmail.com>
2026-01-23 01:32:15 +0300

    feat: Added tokionight.nvim to reset.lua - disabled italic styles.

--- lua/plugins/extras/sakkada/reset.lua
+++ lua/plugins/extras/sakkada/reset.lua
@@ -105,4 +105,30 @@ return {
       },
     },
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      -- Taken from https://github.com/folke/tokyonight.nvim (Default Options)
+      style = "moon", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+      cache = true, -- When set to true, the theme will be cached for better performance
+    },
+  },
 }
