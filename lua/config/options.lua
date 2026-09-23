462ef9f430fd7286aca170b8d8e60ab357495db5
Murat Guchetl <gmurka@gmail.com>
2026-09-23 12:37:18 +0300

    feat: Added config for python lsp and ruff and set language in options.lua.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.cmd.language("en_GB") -- Vim language
+
+-- LSP Server to use for Python.
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_ruff = "ruff"
